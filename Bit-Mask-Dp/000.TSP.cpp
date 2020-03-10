4b2e8a4c5e68b41a8c06a6521607be79f4b0f71c
the-algorist <56862524+the-algorist@users.noreply.github.com>
2020-03-10 20:38:00 +0530

    Create 000.TSP.cpp

---
+++ Bit-Mask-Dp/000.TSP.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define mod 1000000007
+
+int n=4;
+int dist[10][10]={
+      {0,20,42,25},
+      {20,0,30,34},
+      {42,30,0,10},
+      {25,34,10,0}
+};
+
+
+int dp[1<<4][4];
+
+int  visited_all=(1<<n)-1;
+
+int tsp(int mask,int pos){
+    
+    if(mask == visited_all){
+       return dist[pos][0];
+    }
+
+    if(dp[mask][pos]!=-1){
+      return dp[mask][pos];
+    }
+
+    int ans =INT_MAX;
+    for(int city=0;city<n;city++){  
+      if((mask & (1<<city))==0){
+        int new_ans= dist[pos][city] + tsp(mask|(1<<city),city);
+        ans=min(ans,new_ans);
+      }
+    }
+    return dp[mask][pos]=ans;
+
+}
+
+int main(){
+  memset(dp,-1,sizeof dp);
+  cout<<tsp(1,0)<<endl;
+  return 0;
+}
