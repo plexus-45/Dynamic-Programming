ad370008f695d4b188da243f9a390dbfc049a22d
the-algorist <56862524+the-algorist@users.noreply.github.com>
2020-03-26 09:50:22 +0530

    Update 2.Rat&Elephant_ways.cpp

--- Grid-Based-Dp/2.Rat&Elephant_ways.cpp
+++ Grid-Based-Dp/2.Rat&Elephant_ways.cpp
@@ -34,3 +34,35 @@ long long int min_ways(int R,int C){
    return fact(R+C-2)/fact(R-1)*fact(C-1);
 }
 
+//Elephant_version(elephant can take any jump upto(1 to n-1))
+
+int min_ways(int R,int C){
+
+   for(int i=0;i<R;i++){
+      for(int j=0;j<C;j++){
+           
+        if(i==0 and j==0){
+          dp[i][j]=1;
+        }
+        else if(i==0){
+            for(int k=0;k<j;j++){
+               dp[0][j]+=dp[0][k];
+            } 
+        }
+        else if(j==0){
+            for(int k=0;k<i;k++){
+               dp[i][0]+=dp[k][0];
+            }
+        }
+        else{
+            for(int k=0;k<i;k++){
+               dp[i][j]+=dp[k][j];
+            }
+            for(int k=0;k<j;k++){
+               dp[i][j]+=dp[i][k];
+            }   
+        }
+      }
+   }
+   return dp[R-1][C-1];
+}
