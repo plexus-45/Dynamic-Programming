21fa69f6dded8f7df5f3b0410e404660f1bd00fd
the-algorist <56862524+the-algorist@users.noreply.github.com>
2020-04-01 11:25:50 +0530

    Create 7.Flood-fill.cpp

---
+++ Grid-Based-Dp/7.Flood-fill.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int maxN = 5008;
+
+int n;
+int dp[maxN][maxN];
+vector<int> a(1), b;
+vector<int> ans;
+
+int main() {
+	
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x; cin >> x;
+		if (x != a.back()) a.push_back(x);
+	}
+	n = a.size() - 1;
+	b = a;
+	reverse(b.begin() + 1, b.end());
+  
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (a[i] == b[j]) {dp[i][j] = dp[i-1][j-1] + 1;}
+			else {dp[i][j] = max(dp[i-1][j], dp[i][j-1]);}
+		}
+	}
+  
+	cout << n - (dp[n][n] + 1)/2 << '\n';
+	
+	return 0;
+}
